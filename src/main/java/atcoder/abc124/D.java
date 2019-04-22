d9c106870b242580e4135ff9db3b9b7de8be764e
Ryohei UEDA <ueda_ryohei@cyberagent.co.jp>
2019-04-22 09:07:33 +0900

    atcoder.abc124

---
+++ src/main/java/atcoder/abc124/D.java
@@ -0,0 +1,27 @@
+//package atcoder.abc124;
+//
+//import java.util.*;
+//
+//public class D {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int k = sc.nextInt();
+//        char[] charArr = sc.next().toCharArray();
+//        List<Integer> list = new ArrayList<>();
+//        int count = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (charArr[i] == 1) {
+//                count++;
+//            } else {
+//
+//            }
+//        }
+//    }
+//
+//    static void debug(Object... os) {
+//        System.err.println(Arrays.deepToString(os));
+//    }
+//
+//}
