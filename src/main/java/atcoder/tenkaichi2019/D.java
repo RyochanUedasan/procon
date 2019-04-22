c5b37ed15ad18214c7954a20af0074aa0c1645c1
Ryohei UEDA <ueda_ryohei@cyberagent.co.jp>
2019-04-22 09:07:58 +0900

    atcoder.tenkaichi2019

---
+++ src/main/java/atcoder/tenkaichi2019/D.java
@@ -0,0 +1,18 @@
+package atcoder.tenkaichi2019;
+
+import java.util.*;
+
+public class D {
+
+    static long mod = 998244353;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+
+    static void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+}
