effaf1f47795ced05d266db46260c32222d7b30a
Ryohei UEDA <uedaryohei0421@gmail.com>
2019-06-16 20:51:03 +0900

    atcoder.diverta2

---
+++ src/main/java/atcoder/diverta2/A.java
@@ -0,0 +1,24 @@
+package atcoder.diverta2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+
+    void run() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        // write your code
+        System.out.println(k==1 ?  0 : (n - k));
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new A().run();
+    }
+}
---
+++ src/main/java/atcoder/diverta2/D.java
@@ -0,0 +1,33 @@
+package atcoder.diverta2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D {
+
+    void run() {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long root = (long) Math.ceil(Math.sqrt(n));
+
+        long sum = 0;
+
+        for (int x = 1; x < root; x++) {
+            if (n % x == 0) {
+                long m = n / x - 1;
+                if (x < m) sum += m;
+            }
+        }
+        // write your code
+        System.out.println(sum);
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new D().run();
+    }
+}
