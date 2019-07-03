ce90f13c499c79fdee403b09a8e4b9f706c03e89
Ryohei UEDA <ueda_ryohei@cyberagent.co.jp>
2019-07-03 17:14:43 +0900

    atcoder.abc132

---
+++ src/main/java/atcoder/abc132/C.java
@@ -0,0 +1,37 @@
+package atcoder.abc132;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C {
+
+    void run() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dArr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dArr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(dArr);
+
+        int lower = dArr[n / 2 - 1];
+        int upper = dArr[n / 2];
+
+        System.out.println(upper - lower);
+
+
+        // write your code
+//        System.out.println("Hello World");
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new C().run();
+    }
+}
---
+++ src/main/java/atcoder/abc132/E.java
@@ -0,0 +1,20 @@
+package atcoder.abc132;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class E {
+
+    void run() {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new E().run();
+    }
+}
