6f967a73b711a7dbbbb52c1f066e9e0297324ccd
Ryohei <uedaryohei0421@gmail.com>
2020-04-13 10:35:54 +0900

    atcoder.abc162

---
+++ src/main/java/atcoder/abc162/A.java
@@ -0,0 +1,25 @@
+package atcoder.abc162;
+
+import java.util.*;
+
+public class A {
+    void run() {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        boolean ans = false;
+        for (char c : n.toCharArray()) {
+            if (c == '7') {
+                ans = true;
+            }
+        }
+        System.out.println(ans ? "Yes" : "No");
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new A().run();
+    }
+}
