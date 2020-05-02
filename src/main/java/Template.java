2ecfd0707117a506aa388211df665ddf2ddd3284
Ryohei <uedaryohei0421@gmail.com>
2020-05-03 02:06:32 +0900

    modify template

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -1,12 +1,15 @@
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Template {
 
     void run() {
         Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
 
         // write your code
-        System.out.println("Hello World");
+        out.println("Hello World");
+        out.flush();
     }
 
     void debug(Object... os) {
--- src/main/java/atcoder/template/E.java
+++ src/main/java/atcoder/template/E.java
@@ -1,12 +1,17 @@
 package atcoder.template;
 
+import java.io.PrintWriter;
 import java.util.*;
 
 public class E {
     void run() {
         Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
         int n = sc.nextInt();
-    }
+
+        // out.println("Hello World");
+        out.flush();    }
 
     void debug(Object... os) {
         System.err.println(Arrays.deepToString(os));
