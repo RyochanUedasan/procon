e3cf304b2ad20f3e0cac70af1570d9f179dde553
Ryohei <uedaryohei0421@gmail.com>
2020-07-06 22:59:45 +0900

    atcoder.abc169

---
+++ src/main/java/atcoder/abc169/A.java
@@ -0,0 +1,25 @@
+package atcoder.abc169;
+
+import java.io.PrintWriter;
+import java.util.*;
+
+public class A {
+    void run() {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        out.println(a * b);
+        out.flush();
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new A().run();
+    }
+}
---
+++ src/main/java/atcoder/abc169/C.java
@@ -0,0 +1,30 @@
+package atcoder.abc169;
+
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.*;
+import java.math.RoundingMode;
+
+
+public class C {
+    void run() {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        BigDecimal a = sc.nextBigDecimal();
+        BigDecimal b = sc.nextBigDecimal();
+
+        System.out.println(b.multiply(a).setScale(0, RoundingMode.DOWN).longValue());
+
+        // out.println("Hello World");
+        out.flush();
+    }
+
+    void debug(Object... os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+    public static void main(String[] args) {
+        new C().run();
+    }
+}
